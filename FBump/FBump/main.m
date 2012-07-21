8a25c8331923c47e42d516243bc427f01a1c6b0f
Daniel Gur <danielgur2105@gmail.com>
2012-07-21 14:28:22 -0400

    Adding FBump iOS project files

---
+++ FBump/FBump/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBump
+//
+//  Created by Daniel Gur on 7/21/12.
+//  Copyright (c) 2012 Kent State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
