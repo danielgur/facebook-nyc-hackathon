a648fe6b25d2fe875bc47ece7811df6d5014e854
Michelle Luo <michelleluo@college.harvard.edu>
2012-07-21 13:47:41 -0400

    initial commit

---
+++ friendRequest/friendRequest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  friendRequest
+//
+//  Created by Michelle Luo on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ friendRequest/friendRequestTests/friendRequestTests.h
@@ -0,0 +1,13 @@
+//
+//  friendRequestTests.h
+//  friendRequestTests
+//
+//  Created by Michelle Luo on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface friendRequestTests : SenTestCase
+
+@end
